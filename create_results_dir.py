8fec3729ffcd12aff0857a3406aaf7920d9cf4d6
noah1235 <noahfrank2022@gmail.com>
2025-10-17 16:39:00 -0400

    Update create_results_dir.py

--- create_results_dir.py
+++ create_results_dir.py
@@ -1,6 +1,8 @@
 import os
 
 def create_results_dir():
-    root = "E:/NF/KF_Results"
+    #root = "E:/NF/KF_Results"
+    #os.makedirs(root, exist_ok=True)
+    root = "/Users/noahfrank/Desktop/Kolmogorov_flow_LPT/Results"
     os.makedirs(root, exist_ok=True)
     return root
