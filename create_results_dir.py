9698ee7d2fa686b663778368b23dfb231cdb7483
noah1235 <noahfrank2022@gmail.com>
2025-10-17 19:21:40 -0400

    commit

--- create_results_dir.py
+++ create_results_dir.py
@@ -1,8 +1,10 @@
 import os
 
 def create_results_dir():
-    #root = "E:/NF/KF_Results"
-    #os.makedirs(root, exist_ok=True)
     root = "/Users/noahfrank/Desktop/Kolmogorov_flow_LPT/Results"
     os.makedirs(root, exist_ok=True)
+
+    #parent_dir = os.path.dirname(os.getcwd())          # directory above current
+    #root = os.path.join(parent_dir, "Results")         # add 'Results' subdir
+    #os.makedirs(root, exist_ok=True)
     return root
