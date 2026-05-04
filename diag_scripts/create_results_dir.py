bd6da4ddc590f09caef8ea1670155c9bbf7dded7
noah1235 <noahfrank2022@gmail.com>
2026-05-04 14:09:02 -0400

    exclude diag scripts from docker build

--- diag_scripts/create_results_dir.py
+++ diag_scripts/create_results_dir.py
@@ -2,7 +2,7 @@ import os
 
 def create_results_dir():
 
-    with open("../kf_da_configs/data_dir.txt", "r") as f:
+    with open("../kf-da-configs/data_dir.txt", "r") as f:
         root = f.read().rstrip("\n")
     
     return root
